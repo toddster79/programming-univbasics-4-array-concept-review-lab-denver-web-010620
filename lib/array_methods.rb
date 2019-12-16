f9a019a728ef4f7e3e53460ca8a9a2017074743d
Todd Carlson <todd.carlson3030@me.com>
2019-12-16 18:15:50 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,25 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times do |index|
+    if array[index] == value_to_find
+     return index 
+    end 
+  end 
+  puts nil 
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.length.times do |index|
+    return array.max 
+  end 
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.length.times do |index|
+    return array.min 
+  end 
 end
+
+
+
+
+
