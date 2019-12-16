eb01324e79c7402c7be762bbc2207f4bc0b18a04
Todd Carlson <todd.carlson3030@me.com>
2019-12-16 18:03:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,21 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times do |index|
+    if array[index] == value_to_find
+     return index 
+    end 
+  end 
+  puts nil 
 end
 
 def find_max_value(array)
-  # Add your solution here
+  
 end
 
 def find_min_value(array)
-  # Add your solution here
+  
 end
+
+
+
+
+
